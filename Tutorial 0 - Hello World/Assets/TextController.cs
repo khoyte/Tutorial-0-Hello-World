23bc7181011a66c4ddd336f061269f3267e6d029
Kayla H <hoytekayla@gmail.com>
2020-01-16 21:46:46 -0500

    Tutorial 0

---
+++ Tutorial 0 - Hello World/Assets/TextController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    public Text newText;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            newText.text = "Hello, my name is Kayla Hoyte!";
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
